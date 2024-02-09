65faecd0f90a0f00d056b4c308ef4986e07f7659
Asem Gadu <81575109+Asmblxer@users.noreply.github.com>
2024-02-09 22:17:23 +0200

    Update mario.c

--- mario_more/mario.c
+++ mario_more/mario.c
@@ -3,35 +3,33 @@
 
 int main(void)
 {
-     while(true)
-     {
-          int tmp;
-          printf("Hight: ");
-          scanf("%d", &tmp);
-          if(tmp < 1 || tmp > 8)
-          {
-               continue;
-          }
-          else
-          {
-               for(int i = 1; i <= tmp; i++)
-               {
-                    for(int x = 1; x <= tmp - i; x++)
-                    {
-                         printf(" ");
-                    }
-                    for(int j = 1; j <= i; j++)
-                    {
-                         printf("#");
-                    }
-                    printf("  ");
-                    for(int j = 1; j <= i; j++)
-                    {
-                         printf("#");
-                    }
-                    printf("\n");
-               }
-               break;
-          }
-     }
+    while (true)
+    {
+        int tmp = get_int("Height: ");
+        if (tmp < 1 || tmp > 8)
+        {
+            continue;
+        }
+        else
+        {
+            for (int i = 1; i <= tmp; i++)
+            {
+                for (int x = 1; x <= tmp - i; x++)
+                {
+                    printf(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    printf("#");
+                }
+                printf("  ");
+                for (int j = 1; j <= i; j++)
+                {
+                    printf("#");
+                }
+                printf("\n");
+            }
+            break;
+        }
+    }
 }
