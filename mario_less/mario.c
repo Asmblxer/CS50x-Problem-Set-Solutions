be33c544dc875d446aba4fa5a62d972df92fbb4b
Asem Gadu <81575109+Asmblxer@users.noreply.github.com>
2024-02-09 22:16:46 +0200

    Update mario.c

--- mario_less/mario.c
+++ mario_less/mario.c
@@ -15,14 +15,7 @@ int main(void)
             for (int i = 1; i <= tmp; i++)
             {
                 for (int x = 1; x <= tmp - i; x++)
-                {
                     printf(" ");
-                }
-                for (int j = 1; j <= i; j++)
-                {
-                    printf("#");
-                }
-                printf("  ");
                 for (int j = 1; j <= i; j++)
                 {
                     printf("#");
