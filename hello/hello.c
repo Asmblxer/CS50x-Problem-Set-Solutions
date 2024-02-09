b9ac01c5fdceb39eb0389fe28e723ed3a593c959
Asem Gadu <81575109+Asmblxer@users.noreply.github.com>
2024-02-09 22:12:06 +0200

    Update hello.c

--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void) {
-     string nm = get_string("What's your name? ");
-     printf("hello, %s\n", nm);
+int main(void)
+{
+    // Take the user name
+    string name = get_string("What's your name?\n");
+
+    // print hello to user
+    printf("hello, %s\n", name);
 }
