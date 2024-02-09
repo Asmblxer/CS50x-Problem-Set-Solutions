6214822cbdaa4377280818cad8a041216bac0b1d
Asem Gadu <81575109+Asmblxer@users.noreply.github.com>
2024-02-09 22:35:11 +0200

    Update population.c

--- population/population.c
+++ population/population.c
@@ -1,14 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int n = 1, m = 2;
-    while (n < 9) n = get_int("Start size: ");
-    while (m < n) m = get_int("End size: ");
+    while (n < 9)
+        n = get_int("Start size: ");
+    while (m < n)
+        m = get_int("End size: ");
     int ans = 0;
-    while (n < m) {
+    while (n < m)
+    {
         int new = n / 3, pass = n / 4;
-        n += new, n-= pass;
+        n += new, n -= pass;
         ans++;
     }
     printf("Years: %i\n", ans);
