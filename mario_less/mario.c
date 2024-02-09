ba74ba04342d7ad31586fe3896072ca22ed02604
Asem Gadu <81575109+Asmblxer@users.noreply.github.com>
2024-02-09 22:13:34 +0200

    Update mario.c

--- mario_less/mario.c
+++ mario_less/mario.c
@@ -1,22 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)  {
-     while(true) {
-          int tmp;
-          printf("Hight: ");
-          scanf("%d", &tmp);
-          if(tmp < 1 || tmp > 8) continue;
-          else {
-               for(int i = 1; i <= tmp; i++) {
-                    for(int x = 1; x <= tmp - i; x++)
-                         printf(" ");
-                    for(int j = 1; j <= i; j++) {
-                         printf("#");
-                    }
-                    printf("\n");
-               }
-               break;
-          }
-     }
+int main(void)
+{
+    while (true)
+    {
+        int tmp = get_int("Height: ");
+        if (tmp < 1 || tmp > 8)
+        {
+            continue;
+        }
+        else
+        {
+            for (int i = 1; i <= tmp; i++)
+            {
+                for (int x = 1; x <= tmp - i; x++)
+                    printf(" ");
+                for (int j = 1; j <= i; j++)
+                {
+                    printf("#");
+                }
+                printf("\n");
+            }
+            break;
+        }
+    }
 }
